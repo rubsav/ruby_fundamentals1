8ef48d1b57775cb15a7edcbeaf493e103bafb6b6
Ruben Avila <Ruavilaa@hotmail.com>
2014-10-21 14:18:45 -0400

    program asks user name and age returning greeting and birth year

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,8 @@
-puts "What is your name?"
-name = gets.chomp
-puts "Hi #{name}"
+# puts "What is your name?"
+# name = gets.chomp
+# puts "Hi #{name}"
+
+puts "how old are you?"
+age = gets.chomp.to_i #turns input into an integer
+year_born = 2014 - age
+puts "You were born in #{year_born}" #returns the users birth year
