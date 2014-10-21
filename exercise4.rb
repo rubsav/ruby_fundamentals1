1fc3eeede2acf1a57f7ba02600fdb79af4f28d3a
Ruben Avila <Ruavilaa@hotmail.com>
2014-10-21 15:11:13 -0400

    simpler version of the same program

--- exercise4.rb
+++ exercise4.rb
@@ -16,3 +16,18 @@ both= num % 3 & num % 5 == 0
 		puts "Bitmaker"
 end
 end
+
+iteration = (1..100)
+
+iteration.each do |num|
+	if num % 15 == 0
+		puts "Bitmaker"
+	elsif num % 5 == 0
+		puts "Maker"
+	elsif num % 3 == 0
+		puts "Bit"
+	else
+		puts num
+	end
+end
+
