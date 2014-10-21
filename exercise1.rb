c1c746c250e38be3fd74bfc7ad3dedb828252b42
Ruben Avila <Ruavilaa@hotmail.com>
2014-10-21 14:00:49 -0400

    Tip calculation, string to integer & booleans

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,18 @@
 puts 2
 puts 3
 puts 2 != 3
+
+puts "Enter name"
+name = gets.chomp
+puts "Your name is #{name}"
+
+a = 20
+b = 65
+puts "The value of a is #{a}." # The value of a is 20.
+puts "The value of a is #{b}." # The value of a is 65.
+puts "The value of a plus b is #{a + b}." # The value of a plus b is 85.
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
---
+++ exercise2.rb
@@ -0,0 +1,19 @@
+meal_price = 55
+tip = (meal_price * 0.15) #calculates a 15% tip
+	puts "the 15% tip of #{meal_price} is #{tip}"
+
+result = "string".to_i + 2 #turns string into integer
+puts result
+
+a= 45628
+b= 7839
+
+puts "the result of a plus b is equal to {a+b}" 
+	adds the value of a & b through stirng interpolation
+
+value = (true && false) || (false && true) || !(false && false)
+if value #evaluates the expression as true
+	puts true
+else #evaluates the expression as false
+	puts false
+end
